e8f75be86f54e12635c396f0598da15b6f5f433f
Fernandez Mathys <mathys.fernandez5572@gmail.com>
2026-07-08 17:53:31 +0200

    correction de la superposition des étoiles

--- StellarHash/src/univers.rs
+++ StellarHash/src/univers.rs
@@ -47,7 +47,7 @@ fn generer_univers_dynamique(
     let cam_y = camera_transform.translation.y;
     let zoom = camera_transform.scale.x;
 
-    let taille_secteur = 50.0;
+    let taille_secteur = 150.0;
     let rayon_vision = (1000.0 * zoom) as i32 / taille_secteur as i32;
     let rayon_vision = rayon_vision.clamp(10, 100); 
 
@@ -66,36 +66,55 @@ fn generer_univers_dynamique(
             let probabilite = generation::calculer_hachage_spatial(x, y, graine.0);
 
             if probabilite > 0.95 {
-                let systeme_stellaire = crate::astrophysique::generer_caracteristiques(x, y, probabilite);
+                // Le filtre du maximum locale
+                let mut est_le_plus_grand = true;
+                
+                'voisins: for dx in -1..=1 {
+                    for dy in -1..=1 {
+                        if dx == 0 && dy == 0 { continue; }
+                        
+                        let prob_voisin = generation::calculer_hachage_spatial(x + dx, y + dy, graine.0);
+                        if prob_voisin >= probabilite {
+                            est_le_plus_grand = false;
+                            break 'voisins; 
+                        }
+                    }
+                }
 
-                // On choisit la couleur en fonction de la classe spectrale de l'étoile
-                let couleur_etoile = match systeme_stellaire.classe {
-                    crate::astrophysique::ClasseSpectrale::O => Color::srgb(0.3, 0.5, 1.0), // Bleu vif
-                    crate::astrophysique::ClasseSpectrale::B => Color::srgb(0.6, 0.8, 1.0), // Bleu clair
-                    crate::astrophysique::ClasseSpectrale::A => Color::srgb(1.0, 1.0, 1.0), // Blanc
-                    crate::astrophysique::ClasseSpectrale::F => Color::srgb(1.0, 1.0, 0.8), // Jaune paille
-                    crate::astrophysique::ClasseSpectrale::G => Color::srgb(1.0, 0.9, 0.2), // Jaune (Soleil)
-                    crate::astrophysique::ClasseSpectrale::K => Color::srgb(1.0, 0.5, 0.1), // Orange
-                    crate::astrophysique::ClasseSpectrale::M => Color::srgb(0.9, 0.2, 0.2), // Rouge (Naine)
-                };
 
-                // On fait varier la taille visuelle selon le rayon calculé (avec une taille minimum de 8px)
-                let taille_visuelle = 8.0 + (systeme_stellaire.rayon_solaire * 4.0);
+                if est_le_plus_grand {
+                    let systeme_stellaire = crate::astrophysique::generer_caracteristiques(x, y, probabilite);
 
-                // On instancie l'entité avec TOUTES ses données
-                commands.spawn((
-                    SpriteBundle {
-                        sprite: Sprite {
-                            color: couleur_etoile,
-                            custom_size: Some(Vec2::new(taille_visuelle, taille_visuelle)),
+                    let couleur_etoile = match systeme_stellaire.classe {
+                        crate::astrophysique::ClasseSpectrale::O => Color::srgb(0.3, 0.5, 1.0),
+                        crate::astrophysique::ClasseSpectrale::B => Color::srgb(0.6, 0.8, 1.0),
+                        crate::astrophysique::ClasseSpectrale::A => Color::srgb(1.0, 1.0, 1.0),
+                        crate::astrophysique::ClasseSpectrale::F => Color::srgb(1.0, 1.0, 0.8), 
+                        crate::astrophysique::ClasseSpectrale::G => Color::srgb(1.0, 0.9, 0.2),
+                        crate::astrophysique::ClasseSpectrale::K => Color::srgb(1.0, 0.5, 0.1),
+                        crate::astrophysique::ClasseSpectrale::M => Color::srgb(0.9, 0.2, 0.2),
+                    };
+
+                    let taille_visuelle = 8.0 + (systeme_stellaire.rayon_solaire * 4.0);
+
+                    commands.spawn((
+                        SpriteBundle {
+                            sprite: Sprite {
+                                color: couleur_etoile,
+                                custom_size: Some(Vec2::new(taille_visuelle, taille_visuelle)),
+                                ..default()
+                            },
+                            transform: Transform::from_xyz(
+                                (x as f32 * taille_secteur) + (probabilite * 50.0),
+                                (y as f32 * taille_secteur) - (probabilite * 50.0), 
+                                0.0
+                            ),
                             ..default()
                         },
-                        transform: Transform::from_xyz(x as f32 * taille_secteur, y as f32 * taille_secteur, 0.0),
-                        ..default()
-                    },
-                    Etoile,
-                    systeme_stellaire,
-                ));
+                        Etoile,
+                        systeme_stellaire,
+                    ));
+                }
             }
         }
     }
@@ -114,7 +133,7 @@ fn garbage_collector_spatial(
     let cam_y = camera_transform.translation.y;
     let zoom = camera_transform.scale.x;
 
-    let taille_secteur = 50.0;
+    let taille_secteur = 150.0;
     
     // On calcule le rayon de vision actu
     let rayon_vision = (1000.0 * zoom) as i32 / taille_secteur as i32;
@@ -127,19 +146,19 @@ fn garbage_collector_spatial(
     let centre_grille_x = (cam_x / taille_secteur).round() as i32;
     let centre_grille_y = (cam_y / taille_secteur).round() as i32;
 
-    // DÉTRUIRE LES SPRITES
+    // Détruire les entite
     for (entite, transform_etoile) in requete_etoiles.iter() {
         let etoile_x = (transform_etoile.translation.x / taille_secteur).round() as i32;
         let etoile_y = (transform_etoile.translation.y / taille_secteur).round() as i32;
 
-        // Si la distance absolue entre la caméra et l'étoile dépasse notre rayon de despawn alors :
         if (etoile_x - centre_grille_x).abs() > rayon_despawn ||
            (etoile_y - centre_grille_y).abs() > rayon_despawn {
-            
-            // On supprime l'entité du monde Bevy
             commands.entity(entite).despawn();
         }
     }
+
+
+
 
     // VIDER LE CACHE
     // Tout secteur en dehors du rayon de despawn sera définitivement effacé de la mémoire.
