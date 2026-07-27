94abbd2842f5e08acd40f04e2d826f1c31223a6e
Fernandez Mathys <mathys.fernandez5572@gmail.com>
2026-07-27 16:10:51 +0200

    test: Improve a hashing benchmark function

--- benches/generationHashageBench.rs
+++ benches/generationHashageBench.rs
@@ -1,19 +1,35 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
 
 use StellarHash::generation::calculer_hachage_spatial;
 
-fn bench_hachage_spatial(c: &mut Criterion) {
-    // "hachage_spatial_unique" 
 
-    c.bench_function("hachage_spatial", |b| {
-        // b.iter exécute la fonction en boucle des milliers de fois 
-        // pour obtenir une moyenne fiable en nanosecondes
+fn bench_hachage_spatial(c: &mut Criterion) {
+    let graine = 9999;
+
+    // Une seule coordonnée statique (Temps brut de l'algorithme)
+    c.bench_function("hachage_spatial_unique", |b| {
         b.iter(|| {
             calculer_hachage_spatial(
-                black_box(42),    // Coordonnée X
-                black_box(-84),   // Coordonnée Y
-                black_box(9999)   // Graine globale
+                black_box(42), 
+                black_box(-84), 
+                black_box(graine),
             )
+        })
+    });
+
+    // Une grille de 10x10 (Simulation réaliste avec effet de cache processeur)
+    c.bench_function("hachage_spatial_grille_10x10", |b| {
+        b.iter(|| {
+            // On itère sur 100 cases (10 * 10)
+            for x in 0..10 {
+                for y in 0..10 {
+                    black_box(calculer_hachage_spatial(
+                        black_box(x), 
+                        black_box(y), 
+                        black_box(graine)
+                    ));
+                }
+            }
         })
     });
 }
